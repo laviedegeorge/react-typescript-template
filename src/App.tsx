fe443926f0cec68aaa70da6665ff0df336343bed
Kelechi Apugo <laviedegeorge1292@gmail.com>
2023-02-14 10:31:22 +0100

    install prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-3xl text-red-400 font-bold underline">
-      Hello world! Happy val's day
-    </h1>
+        Hello world! Happy val's day
+      </h1>
     </div>
   );
 }
